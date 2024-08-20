5289ebbe6c65bf443a00e6b8804253b15a783007
kuldeep shakya <127841576+KuldeepShakya1159@users.noreply.github.com>
2024-08-20 15:05:07 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT;
 const BannerRoutes = require('./Routes/bannerRoute')
 app.use(express.json());
 app.use(cors({
-    origin:['https://tuf-assignmentfrontend-kuldeep-shakyas-projects.vercel.app'],
+    origin:['https://tuf-assignmentfrontend-521wia8u2-kuldeep-shakyas-projects.vercel.app','https://tuf-assignmentfrontend-kuldeep-shakyas-projects.vercel.app'],
     methods:["GET","POST","PUT","DELETE"]
 }))
 
